81d8224b07df94d7f36e65c2d531b5e5b1b89259
Ryan <39573308+RyanThen@users.noreply.github.com>
2022-04-29 17:49:01 -0500

    initial commit of data file

---
+++ dynamic-text/dynamic-text-data.js
@@ -0,0 +1,53 @@
+//*** COURSE DATA ***//
+
+const courseInfo1 = [
+  { 
+    courseDate1: new Date('4/8/2022'),
+    courseDate2: new Date('4/12/2022'),
+    time: '12:00 PM a 2:00 PM'
+  },
+  {
+    courseDate1: new Date('10/18/2022'),
+    courseDate2: false,
+    time: '11:00 AM a 1:00 PM'
+  }
+];
+
+const courseInfo2 = [
+  { 
+    courseDate1: new Date('5/8/2022'),
+    courseDate2: new Date('5/12/2022'),
+    time: '12:00 PM a 4:00 PM'
+  },
+  {
+    courseDate1: new Date('10/28/2022'),
+    courseDate2: false,
+    time: '11:00 AM a 3:00 PM'
+  }
+];
+
+const courseInfo3 = [
+  { 
+    courseDate1: new Date('9/28/2022'),
+    courseDate2: new Date('10/2/2022'),
+    time: '12:00 PM a 2:00 PM'
+  },
+  {
+    courseDate1: new Date('11/18/2022'),
+    courseDate2: false,
+    time: '11:00 AM a 1:00 PM'
+  }
+];
+
+const courseInfo4 = [
+  { 
+    courseDate1: new Date('11/19/2022'),
+    courseDate2: new Date('12/1/2022'),
+    time: '12:00 PM a 4:00 PM'
+  },
+  {
+    courseDate1: new Date('12/11/2022'),
+    courseDate2: false,
+    time: '11:00 AM a 3:00 PM'
+  }
+];
