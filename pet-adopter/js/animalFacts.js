c83d2161d55564908a81f94ded113562d7b4d7f2
Ryan <39573308+RyanThen@users.noreply.github.com>
2022-06-25 11:27:47 -0500

    started building learn more breed cards

--- pet-adopter/js/animalFacts.js
+++ pet-adopter/js/animalFacts.js
@@ -3,11 +3,28 @@ Set up form to generate info cards where users
 can learn more about various animals/breeds.
 // ------------------------------------------ */
 
-// The Dog API Call
-fetch('https://api.thedogapi.com/v1/breeds/search?q=collie', { headers: { "x-api-key": theDogApiKey } })
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(err => alert(err));
+const learnMoreContainer = document.querySelector('.learn-more-container');
+const learnMoreSelectorContainer = document.querySelector('.learn-more-selector-container');
+const learnMoreSelect = document.querySelector('#learn-more-select');
+
+const buildLearnMoreInfoCard = function() {
+
+  // ######################################################################################################
+  // USE BELOW API CALLS TO BUILD LEARN MORE BREED INFO CARD SO USERS CAN LEARN ABOUT BREED CHARACTERISTICS
+  // ######################################################################################################
+
+  // The Dog API -- Search Specific Dog Breed
+  fetch('https://api.thedogapi.com/v1/breeds/search?q=collie', { headers: { "x-api-key": theDogApiKey } })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(err => alert(err));
+
+  // The Dog API -- Get Image of Dog Breed
+  fetch('https://api.thedogapi.com/v1/images/A09F4c1qP', { headers: { "x-api-key": theDogApiKey } })
+    .then(response => response.json())
+    .then(data => console.log(data.url))
+    .catch(err => alert(err));
+}
 
 
 
